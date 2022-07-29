ab1c72afa94f930155c9c82b588fc941642a6d4b
Kartik213 <kartikg02013@gmail.com>
2022-07-29 17:44:40 +0530

    WeatherApp commit

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ window.addEventListener("load", () => {
             // API URL
             const base =
                 `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
-                `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
+                `lon=${lon}&appid=0c2d3bb3b8706729d8f07156bc4c2901`;
 
             // Calling the API
             fetch(base)
